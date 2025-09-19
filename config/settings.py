36d442709fbbe1693b5b8d62a65e1823ad8bd971
funkylemonh2o <150464518+funkylemonh2o@users.noreply.github.com>
2025-09-19 18:23:24 +0200

    finished project

--- config/settings.py
+++ config/settings.py
@@ -9,23 +9,23 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
-
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv (BASE_DIR/".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xo+)o-s#n#jxfj9g_c(u6el&5t=u=771agr1mt$!!h*$c_pwt)'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG")=="True"
 
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(", ")
 
 
 # Application definition
